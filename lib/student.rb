21b49de8457a8d63c19285046a6141ec9fc4205b
Kateryna Strough <katerynastrough@hotmail.com>
2019-06-13 23:04:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,16 @@
 class Student < User 
   
-attr_accessor :knowledge 
+  attr_accessor :knowledge
 
-def initialize
-  @konwledge = []
-end 
+  def initialize
+    @knowledge = []
+  end
 
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
